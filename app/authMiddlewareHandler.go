597d65605cc47116c6c8de3a7594816b3306f0f0
mrmtsu <81202muramatsu@gmail.com>
2021-06-08 07:49:05 +0900

    jwt認証

--- app/authMiddlewareHandler.go
+++ app/authMiddlewareHandler.go
@@ -1,34 +1,42 @@
 package app
 
 import (
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/dgrijalva/jwt-go"
 )
 
 func IsAuthenticated(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		_, err := r.Cookie("jwt")
-		if err != nil {
+		// _, err := r.Cookie("jwt")
+		// if err != nil {
+		// 	http.Error(w, "No admin cookie", http.StatusForbidden)
+		// 	return
+		// }
+		// next.ServeHTTP(w, r)
+		cookie, err := r.Cookie("jwt")
+     if err != nil {
+          log.Fatal("Cookie: ", err)
+     }
+		if _, err := ParseJwt(cookie.Value); err != nil {
 			http.Error(w, "No admin cookie", http.StatusForbidden)
-			return
 		}
 		next.ServeHTTP(w, r)
-		// cookie, _ := r.Cookie("jwt")
-		// if _, err := ParseJwt(cookie.Value); err != nil {
-		// 	http.Error(w, "No admin cookie", http.StatusForbidden)
-		// }
 	})
 }
 
-// func ParseJwt(cookie string) (string, error) {
-// 	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(t *jwt.Token) (interface{}, error) {
-// 		return []byte(os.Getenv("SIGNINGKEY")), nil
-// 	})
+func ParseJwt(cookie string) (string, error) {
+	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(t *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("SIGNINGKEY")), nil
+	})
 
-// 	if err != nil || !token.Valid {
-// 		return "", err
-// 	}
+	if err != nil || !token.Valid {
+		return "", err
+	}
 
-// 	claims := token.Claims.(*jwt.StandardClaims)
+	claims := token.Claims.(*jwt.StandardClaims)
 
-// 	return claims.Issuer, nil
-// }
+	return claims.Issuer, nil
+}
